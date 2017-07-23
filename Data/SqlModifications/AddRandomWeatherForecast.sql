ab4d78a2af868bf9011043a876ac0f3e993900e8
flo <f.muench@hotmail.com>
2017-07-23 13:04:47 +0200

    - Add database scipt

---
+++ Data/SqlModifications/AddRandomWeatherForecast.sql
@@ -0,0 +1,7 @@
+﻿declare @id int 
+select @id = 8
+while @id >=1 and @id <= 1000
+begin
+    insert into WeatherForecast (DateFormatted, TemperatureC, TemperatureF, Summary) values('1/11/2017', ABS(CHECKSUM(NewId())) % 40, 15, 'strange')
+    select @id = @id + 1
+end
